c6469ea12dfc8a5fad16e9957d060f53de8eaeef
Cédric FONTAINE <cf@eg0.me>
2019-01-27 15:58:45 +0100

    jwt & helper method

--- api/check/index.js
+++ api/check/index.js
@@ -1,10 +1,11 @@
 const getJsonBody = require('../../utils/getJsonBody')
 const sendJson = require('../../utils/sendJson')
+const jwt = require('jsonwebtoken')
 
 module.exports = async (req, res) => {
   switch (req.method) {
     case 'DELETE':
-      sendJsonResponse(
+      sendJson(
         res,
         200,
         { ok: true },
@@ -15,13 +16,19 @@ module.exports = async (req, res) => {
         }
       )
       break
-    case 'POST':
+    case 'POST': {
       const data = await getJsonBody(req)
       if (
         data.login === process.env.ADMIN_EMAIL &&
         data.password === process.env.ADMIN_PASSWORD
       ) {
-        console.log('ooo')
+        const token = jwt.sign(
+          {
+            ihaveno: 'claims'
+          },
+          process.env.JWT_SECRET,
+          { algorithm: 'HS256', expiresIn: '2 days' }
+        )
         sendJson(
           res,
           200,
@@ -29,13 +36,14 @@ module.exports = async (req, res) => {
           {
             'Set-Cookie': `${
               process.env.COOKIE_NAME
-            }=1; HttpOnly; Secure; Max-Age=86400; Path=/`
+            }=${token}; HttpOnly; Secure; Max-Age=86400; Path=/`
           }
         )
       } else {
         sendJson(res, 401, { ok: false })
       }
       break
+    }
     default:
       sendJson(res, 401, { ok: false })
   }
--- api/db/categories.js
+++ api/db/categories.js
@@ -2,57 +2,59 @@ const db = require('../../utils/getFirestore')
 const getJsonBody = require('../../utils/getJsonBody')
 const sendJson = require('../../utils/sendJson')
 const url = require('url')
+const checkJWT = require('../verify')
 
 module.exports = async (req, res) => {
-  console.log(req.method)
-  try {
-    const { query } = url.parse(req.url, true)
-    switch (req.method) {
-      case 'GET':
-        {
-          const querySnapshot = await db
-            .collection('categories')
-            .limit(25)
-            .get()
-          const data = querySnapshot.docs.map(documentSnapshot => {
-            return { ...documentSnapshot.data(), id: documentSnapshot.id }
-          })
-          sendJson(res, 200, data)
-        }
-        break
-      case 'DELETE':
-        {
-          const documentReference = await db
-            .collection('categories')
-            .doc(query.id)
-          await documentReference.delete()
-          sendJson(res, 200, { ok: true })
-        }
-        break
-      case 'PUT':
-        {
-          const data = await getJsonBody(req)
-          const documentReference = await db
-            .collection('categories')
-            .doc(query.id)
-          await documentReference.update(data)
-          sendJson(res, 200, { ok: true })
-        }
-        break
-      case 'POST':
-        {
-          const data = await getJsonBody(req)
-          const documentReference = await db.collection('categories').add({
-            name: data.name,
-            description: data.description
-          })
-          sendJson(res, 200, { id: documentReference.id })
-        }
-        break
-      default:
-        sendJson(res, 400, 'Method not supported')
+  if (req.method === 'GET' || checkJWT(req, res)) {
+    try {
+      const { query } = url.parse(req.url, true)
+      switch (req.method) {
+        case 'GET':
+          {
+            const querySnapshot = await db
+              .collection('categories')
+              .limit(25)
+              .get()
+            const data = querySnapshot.docs.map(documentSnapshot => {
+              return { ...documentSnapshot.data(), id: documentSnapshot.id }
+            })
+            sendJson(res, 200, data)
+          }
+          break
+        case 'DELETE':
+          {
+            const documentReference = await db
+              .collection('categories')
+              .doc(query.id)
+            await documentReference.delete()
+            sendJson(res, 200, { ok: true })
+          }
+          break
+        case 'PUT':
+          {
+            const data = await getJsonBody(req)
+            const documentReference = await db
+              .collection('categories')
+              .doc(query.id)
+            await documentReference.update(data)
+            sendJson(res, 200, { ok: true })
+          }
+          break
+        case 'POST':
+          {
+            const data = await getJsonBody(req)
+            const documentReference = await db.collection('categories').add({
+              name: data.name,
+              description: data.description
+            })
+            sendJson(res, 200, { id: documentReference.id })
+          }
+          break
+        default:
+          sendJson(res, 400, 'Method not supported')
+      }
+    } catch (err) {
+      sendJson(res, 400, { err })
     }
-  } catch (err) {
-    sendJson(res, 400, { err })
   }
 }
